0a3e8ec6a5f9283fe9d38d46b8c0baecd4133cb1
arnav dhamija <arnav.dhamija@gmail.com>
2017-10-25 09:23:51 +0530

    Working on fragment

--- Bromide/app/src/main/java/com/arnavdhamija/bromide/MainActivity.java
+++ Bromide/app/src/main/java/com/arnavdhamija/bromide/MainActivity.java
@@ -1,11 +1,15 @@
 package com.arnavdhamija.bromide;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button bromideButton = (Button) findViewById(R.id.bromide_button);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final String hubAddress = sharedPreferences.getString("hub_address", "");
+
+        bromideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("BROMIDE", "HUB_ADDR"+hubAddress);
+            }
+        });
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "TBD", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
-
 }
